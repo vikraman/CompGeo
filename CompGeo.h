e1fe9b2ab213746aa4072e5013d6d18a9b469eaf
Vikraman Choudhury <vikraman.choudhury@gmail.com>
2011-01-18 01:09:26 +0530

    started out with first header

---
+++ CompGeo.h
@@ -0,0 +1,21 @@
+/*
+ * CompGeo.h
+ *
+ * @author vh4x0r
+ */
+
+#ifndef __COMPGEO_H__
+#define __COMPGEO_H__
+
+typedef struct _Point2D {
+  double x;
+  double y;
+} Point2D;
+
+typedef struct _Line2D {
+  Point2D p1;
+  Point2D p2;
+} Line2D;
+
+#endif
+/* CompGeo.h */
