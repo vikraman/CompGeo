02523a747ae98075dad0ccd4bf7a68a82b37f0f5
Vikraman Choudhury <vikraman.choudhury@gmail.com>
2011-01-20 22:42:26 +0530

    tests for exhaustive segment intersection

--- tests/testSegmentIntersection.c
+++ tests/testSegmentIntersection.c
@@ -3,21 +3,14 @@
 #include "SegmentIntersection.h"
 
 int main () {
-  Line2D l[3];
-
-  l[0].p1.x = 0; l[0].p1.y = 0;
-  l[0].p2.x = 3; l[0].p2.y = 3;
-
-  l[1].p1.x = 1; l[1].p1.y = 2;
-  l[1].p2.x = 4; l[1].p2.y = 1;
-
-  l[2].p1.x = 2; l[2].p1.y = 1;
-  l[2].p2.x = 4; l[2].p2.y = 4;
-
+  Line2D l;
   initSegmentIntersection ();
-  addSegment (&l[0]);
-  addSegment (&l[1]);
-  addSegment (&l[2]);
+  puts ("Input x1, y1, x2, y2 ...");
+  do {
+    scanf ("%lf %lf %lf %lf", &l.p1.x, &l.p1.y, &l.p2.x, &l.p2.y);
+    if (l.p1.x == l.p2.x && l.p1.y == l.p2.y) break;
+    addSegment (&l);
+  } while (1);
   Point2DList p;
   findIntersection (&p);
 
